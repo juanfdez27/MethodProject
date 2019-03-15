65331d12570292d57832b6279b59fd083aa74fc1
juan <juan.fernandez.doremire.2020@student.lhschools.org>
2019-03-15 07:36:51 -0400

    Hello World + Hello "Name" (2 commits)

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    public static void print(){
+        Scanner keyboard;
+        String name;
+        keyboard = new Scanner(System.in);
+        name = keyboard.nextLine();
+        System.out.println("Hello" + name);
+
+
+    }
+
+
+
+    }
+
